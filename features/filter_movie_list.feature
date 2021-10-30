70f62df009233793d8dd34f579646b386f90e850
Ethan Chen <ethanyxchen@berkeley.edu>
2021-10-30 01:18:07 +0000

    completed part 1,2,3

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,14 +23,22 @@ Background: movies have been added to database
   Then 10 seed movies should exist
 
 Scenario: restrict to movies with "PG" or "R" ratings
-  And I check the "PG" checkbox
-  Then complete the rest of of this scenario
+  # And I check the "PG" checkbox
+  # Then complete the rest of of this scenario
   # enter step(s) to check the "PG" and "R" checkboxes
   # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
+  # enter step to "submit" the search form on the homepage  
+  # enter step(s) to ensure that PG and R movies are visible  
   # enter step(s) to ensure that other movies are not visible
+    
+  Given I check the following ratings: PG, R
+  And I uncheck the following ratings: G, PG-13
+  And I press "Refresh"
+  Then I should see the following movies: Amelie, Raiders of the Lost Ark, The Incredibles, The Terminator, When Harry Met Sally
+
 
 Scenario: all ratings selected
   # your steps here
-  Then complete the rest of of this scenario
+  Given I check the following ratings: G, PG, PG-13, R
+  Then I should see all the movies
+  
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,15 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+  
+      Movie.create(:title => movie[:title], 
+          :rating => movie[:rating],
+          :description => movie[:description],
+          :release_date => movie[:release_date],
+          :created_at => movie[:created_at],
+          :updated_at => movie[:updated_at])
+  
   end
-  pending "Fill in this step in movie_steps.rb"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +25,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  pending "Fill in this step in movie_steps.rb"
+    page.body.should =~ /#{e1}.*#{e2}/m
+    
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,18 +37,34 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+  rating_list.split(", ").each do |rating|
+      if uncheck
+          steps %Q{ When I uncheck the "#{rating}" checkbox }
+      else 
+          steps %Q{ When I check the "#{rating}" checkbox }
+      end 
+  end 
+    
 end
 
 # Part 2, Step 3
 Then /^I should (not )?see the following movies: (.*)$/ do |no, movie_list|
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+  movie_list.split(", ").each do |movie|
+      if no 
+          steps %Q{ Then I should not see "#{movie}"}
+      else 
+          steps %Q{ Then I should see "#{movie}"}
+      end 
+  end 
+    
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
+  rows = page.all('#movies tbody tr').size
+  expect(rows).to eq 10
+  
 end
 
 ### Utility Steps Just for this assignment.
